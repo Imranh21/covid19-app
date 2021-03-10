af83a849696c7e56e716cbb3d51413e393735e8e
Imran <44378535+Imranh21@users.noreply.github.com>
2021-03-10 12:52:51 +0600

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import ReactMapGL, { Marker } from "react-map-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import styles from "./Header.module.css";
 import RedMarker from "./RedMarker";
 
